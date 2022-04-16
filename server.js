986b3ae01aad915f0de819f40c9e49711510777a
mariia <mskorik@myseneca.ca>
2022-04-16 01:18:32 -0400

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(__dirname + '/dist/bti425-a3-mariiaskorik'));
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<bti425-a3-mariiaskorik>'));
+
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname + '/dist/bti425-a3-mariiaskorik/index.html'));});
+    
+res.sendFile(path.join(__dirname+'/dist/<bti425-a3-mariiaskorik>/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
